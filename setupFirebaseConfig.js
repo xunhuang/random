e4fa01d57652142c9415964bab23ad87b14457e5
Wilson Huang (Growth) <xhuang@fb.com>
2021-01-11 15:42:48 -0800

    adding temp

---
+++ setupFirebaseConfig.js
@@ -0,0 +1,18 @@
+// 
+// before running me you should do a "npm install firebase-tools" in current directory
+//
+const fbcli = require('firebase-tools');
+const fs = require('fs');
+
+/*
+fbcli.setup.web().then(config => {
+  fs.writeFileSync(
+    'src/firebaseConfig.json',
+    JSON.stringify(config, 2, 2),
+  );
+});
+*/
+
+fbcli.apps.sdkconfig().then(config=> {
+    console.log(config);
+});
