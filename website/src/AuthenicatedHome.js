01a108578e587ed2898fd47910598d64bc38b24c
Wilson Huang (Growth) <xhuang@fb.com>
2021-03-09 18:25:58 -0800

    basic add/edit/delete for watch subscriptions

--- website/src/AuthenicatedHome.js
+++ website/src/AuthenicatedHome.js
@@ -1,10 +1,77 @@
-import { jsxs as _jsxs, jsx as _jsx } from "react/jsx-runtime";
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
 import React from 'react';
 import { RandomBackend } from "./RandomBackend";
 import { WatchSubscription } from "./AuthUser";
 import MUIDataTable from 'mui-datatables';
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+function SubscriptionForm(props) {
+    let user = RandomBackend.getCurrentUser();
+    const schema = yup.object().shape({
+        name: yup.string().required(),
+        url: yup.string().url().required(),
+    });
+    const { register, handleSubmit, errors, reset } = useForm({
+        resolver: yupResolver(schema)
+    });
+    const onSubmit = handleSubmit(({ name, url }) => {
+        let sub = props.sub || new WatchSubscription();
+        sub.name = name;
+        sub.url = url;
+        if (!props.sub) {
+            user.subscriptions.create(sub).then(function () {
+                console.log("done creating subscriptions");
+                if (props.callback) {
+                    props.callback();
+                }
+                reset();
+            });
+        }
+        else {
+            user.subscriptions.update(sub).then(function () {
+                console.log("done updating subscriptions");
+                if (props.callback) {
+                    props.callback();
+                }
+            });
+        }
+    });
+    let sub = props.sub;
+    return (_jsxs("div", { children: [sub ?
+                _jsx("label", { children: "Update Subscription" }, void 0) :
+                _jsx("label", { children: "New Subscription" }, void 0),
+            _jsxs("form", Object.assign({ onSubmit: onSubmit }, { children: [_jsx("label", { children: "Name" }, void 0),
+                    _jsx("input", { name: "name", ref: register, defaultValue: sub && sub.name }, void 0), errors.name && "Name is required", _jsx("label", { children: "URL" }, void 0),
+                    _jsx("input", { name: "url", ref: register, defaultValue: sub && sub.url }, void 0), errors.url && "URL must be valid", _jsx("input", { type: "submit" }, void 0)] }), void 0)] }, void 0));
+}
+const SubscripionList = (props) => {
+    const columns = [
+        { label: 'Title', name: 'name' },
+        { label: 'Author', name: 'url' },
+    ];
+    const options = {
+        onRowsDelete: (rowsDeleted) => {
+            let user = RandomBackend.getCurrentUser();
+            rowsDeleted.data.map(d => {
+                let subid = props.subs[d.dataIndex].id;
+                user.subscriptions.delete(subid).then(() => {
+                    console.log("deleted:" + subid);
+                });
+            });
+        },
+        onRowClick: (rowData, rowMeta) => {
+            if (props.subClicked) {
+                console.log("row clicked");
+                props.subClicked(props.subs[rowMeta.dataIndex]);
+            }
+        }
+    };
+    return (_jsx("div", Object.assign({ style: { maxWidth: '100%' } }, { children: _jsx(MUIDataTable, { columns: columns, data: props.subs, title: 'Watch Subscriptions', options: options }, void 0) }), void 0));
+};
 export const AuthenicatedHome = () => {
     let user = RandomBackend.getCurrentUser();
+    const [selectedSub, setSelectedSub] = React.useState(undefined);
     const [subs, setSubs] = React.useState(undefined);
     const [reload, setReload] = React.useState(false);
     React.useEffect(() => {
@@ -12,23 +79,14 @@ export const AuthenicatedHome = () => {
             setSubs(data);
         });
     }, [reload]);
-    const columns = [
-        { label: 'Title', name: 'name' },
-        { label: 'Author', name: 'url' },
-    ];
-    const options = {
-        filterType: 'checkbox'
-    };
     return _jsxs("div", { children: [_jsxs("h1", { children: ["AuthenticatedHome - ", user.displayName, ", ", user.id] }, void 0),
-            _jsx("div", Object.assign({ style: { maxWidth: '100%' } }, { children: _jsx(MUIDataTable, { columns: columns, data: subs, title: 'Watch Subscriptions' }, void 0) }), void 0),
-            _jsx("p", Object.assign({ onClick: (event) => {
-                    let user = RandomBackend.getCurrentUser();
-                    let sub = new WatchSubscription();
-                    sub.name = "hey hey";
-                    sub.url = "https://cnn.com";
-                    user.subscriptions.create(sub).then(function () {
-                        console.log("done creating subscriptions");
-                        setReload(!reload);
-                    });
-                } }, { children: "Add Subscription" }), void 0)] }, void 0);
+            _jsx(SubscriptionForm, { sub: selectedSub, callback: () => {
+                    console.log("hello");
+                    setSelectedSub(undefined);
+                    setReload(!reload);
+                } }, void 0),
+            _jsx(SubscripionList, { subs: subs, subClicked: sub => {
+                    console.log("upldated selected sub");
+                    setSelectedSub(sub);
+                } }, void 0)] }, void 0);
 };
--- website/src/AuthenicatedHome.tsx
+++ website/src/AuthenicatedHome.tsx
@@ -2,9 +2,113 @@ import React from 'react';
 import { RandomBackend } from "./RandomBackend";
 import { WatchSubscription } from "./AuthUser";
 import MUIDataTable from 'mui-datatables';
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+interface SubscriptionFormProperty {
+    sub?: WatchSubscription;
+    callback?: () => void;
+}
+
+function SubscriptionForm(props: SubscriptionFormProperty) {
+    let user = RandomBackend.getCurrentUser();
+    type FormData = {
+        name: string;
+        url: string;
+    };
+    const schema = yup.object().shape({
+        name: yup.string().required(),
+        url: yup.string().url().required(),
+    });
+    const { register, handleSubmit, errors, reset } = useForm<FormData>({
+        resolver: yupResolver(schema)
+    });
+
+    const onSubmit = handleSubmit(({ name, url }) => {
+        let sub = props.sub || new WatchSubscription();
+        sub.name = name;
+        sub.url = url;
+        if (!props.sub) {
+            user.subscriptions.create(sub).then(function () {
+                console.log("done creating subscriptions");
+                if (props.callback) { props.callback(); }
+                reset();
+            });
+        } else {
+            user.subscriptions.update(sub).then(function () {
+                console.log("done updating subscriptions");
+                if (props.callback) { props.callback(); }
+            });
+        }
+    });
+
+    let sub = props.sub;
+    return (
+        <div>
+            {
+                sub ?
+                    <label>Update Subscription</label> :
+                    <label>New Subscription</label>
+            }
+            <form onSubmit={onSubmit}>
+                <label>Name</label>
+                <input name="name" ref={register} defaultValue={sub && sub.name} />
+                {/* // <input name="name" ref={register} value={""} /> */}
+                {errors.name && "Name is required"}
+                <label>URL</label>
+                <input name="url" ref={register} defaultValue={sub && sub.url} />
+                {errors.url && "URL must be valid"}
+                <input type="submit" />
+            </form>
+        </div>
+    );
+}
+
+interface SubscriptionListProperty {
+    subs: WatchSubscription[];
+    subClicked?: (sub: WatchSubscription) => void;
+}
+
+const SubscripionList = (props: SubscriptionListProperty) => {
+    const columns = [
+        { label: 'Title', name: 'name' },
+        { label: 'Author', name: 'url' },
+    ];
+
+    const options = {
+        onRowsDelete: (rowsDeleted: { data: any[]; }) => {
+            let user = RandomBackend.getCurrentUser();
+            rowsDeleted.data.map(d => {
+                let subid = props.subs[d.dataIndex].id;
+                user.subscriptions.delete(subid).then(() => {
+                    console.log("deleted:" + subid);
+                })
+            });
+        },
+        onRowClick: (rowData: string[], rowMeta: { dataIndex: number, rowIndex: number }) => {
+            if (props.subClicked) {
+                console.log("row clicked")
+                props.subClicked(props.subs[rowMeta.dataIndex]);
+            }
+        }
+    };
+
+    return (
+        <div style={{ maxWidth: '100%' }}>
+            <MUIDataTable
+                columns={columns}
+                data={props.subs}
+                title='Watch Subscriptions'
+                options={options}
+            />
+        </div>);
+};
 
 export const AuthenicatedHome = () => {
     let user = RandomBackend.getCurrentUser();
+
+    const [selectedSub, setSelectedSub] = React.useState<WatchSubscription | undefined>(undefined);
     const [subs, setSubs] = React.useState<any>(undefined);
     const [reload, setReload] = React.useState<boolean>(false);
     React.useEffect(() => {
@@ -13,38 +117,19 @@ export const AuthenicatedHome = () => {
         });
     }, [reload]);
 
-    const columns = [
-        { label: 'Title', name: 'name' },
-        { label: 'Author', name: 'url' },
-        // { label: 'Page Count', name: 'num_pages', options: { sort: true } },
-        // { label: 'Rating', name: 'rating' }
-    ];
-    const options = {
-        filterType: 'checkbox'
-    };
-
     return <div>
         <h1>
             AuthenticatedHome - {user.displayName}, {user.id}
         </h1>
-        <div style={{ maxWidth: '100%' }}>
-            <MUIDataTable
-                columns={columns}
-                data={subs}
-                title='Watch Subscriptions'
-            />
-        </div>
-        <p onClick={(event) => {
-            let user = RandomBackend.getCurrentUser();
-            let sub = new WatchSubscription();
-            sub.name = "hey hey";
-            sub.url = "https://cnn.com";
-            user.subscriptions.create(sub).then(function () {
-                console.log("done creating subscriptions");
+        <SubscriptionForm sub={selectedSub}
+            callback={() => {
+                console.log("hello");
+                setSelectedSub(undefined);
                 setReload(!reload);
-            });
-        }}>
-            Add Subscription
-        </p>
+            }} />
+        <SubscripionList subs={subs} subClicked={sub => {
+            console.log("upldated selected sub");
+            setSelectedSub(sub);
+        }} />
     </div >;
 };
