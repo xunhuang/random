e2774d8e6f1bf69b3e1433bef1d23c098232ae1c
Xun Huang <xhuang@gmail.com>
2021-03-20 22:34:21 +0000

    recreate view using scripts

--- bigquery/createviews.sh
+++ bigquery/createviews.sh
@@ -1,4 +1,15 @@
 
-bq rm -f my_dataset.myview
-query=`cat CDC-county-test-all.sql`    
-bq mk --use_legacy_sql=false --view "$query"  my_dataset.myview
+
+recreate_view() {
+    view=$1
+    sqlfile=$2
+    bq rm -f $view
+    query=`cat $sqlfile`    
+    bq mk --use_legacy_sql=false --view "$query"  $view
+    bq query --use_legacy_sql=false "select count(*) as count from \`$view\`"
+}
+
+recreate_view my_dataset.CDC-county-test-all CDC-county-test-all.sql
+recreate_view my_dataset.CDC-county-test-latest CDC-county-test-latest.sql
+
+
