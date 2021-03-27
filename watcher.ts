0c28bb4d5ea1260a10a8b99e00bad27fa42812a6
Wilson Huang (Growth) <xhuang@fb.com>
2021-03-27 13:12:04 -0700

    let ingest do the work

--- watcher.ts
+++ watcher.ts
@@ -192,6 +192,15 @@ class Subscription {
 
 const NewSubscriptions = [
     new Subscription(
+        "JHU Current Realtime Cases data",
+        "https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases_US/FeatureServer/0/query?f=json&where=(Confirmed%20%3E%200)%20AND%20(1%3D1)&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=OBJECTID%20ASC&resultOffset=0&resultRecordCount=4000&cacheHint=true&quantizationParameters=%7B%22mode%22%3A%22edit%22%7D",
+        ["xhuang@gmail.com"],
+        {
+            storageTableName: "JHU-ESRI-Realtime",
+            jqQuery: ".features  | .[] | .attributes",
+        }
+    ),
+    new Subscription(
         "LA Times Vaccine Info",
         "https://www.latimes.com/projects/california-coronavirus-cases-tracking-outbreak/covid-19-vaccines-distribution/",
         ["xhuang@gmail.com"],
