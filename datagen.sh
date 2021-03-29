d18cf53b19cdd5edcea953353a80aea005c45519
Wilson Huang (Growth) <xhuang@fb.com>
2021-03-29 15:05:26 -0700

    fixed county path

--- datagen.sh
+++ datagen.sh
@@ -72,20 +72,12 @@ getLatestCovidData() {
    curl -o covidlatest.json "$URLNewCasesJHU"
    cat covidlatest.json | jq -c " .features  | .[] | .attributes" > covidlatest-nl.json
    node loadJsonFromGCS.js  my_dataset ESRI_imported_raw covidlatest-nl.json covidlatest.schema.json
-   # Dont' do this, takes forever, and it's 200M data per run
-   # node BigQuery.js -t my_dataset.Covid-cases-all > covid-all.json
-   # download the last day, validated on bigtable side.
-   # in the future we  might replace the following 3 days with the direct download to
-   # see if it works better.
-   #node BigQuery.js -q 'select county_fips_code as fips, FORMAT_DATE("%F", date) as date , county, state_name as state, confirmed_cases as cases, deaths from  `my_dataset.ESRI-Covid-Lastday`'  > last.json
-   #npx csvtojson covid-19-data/us-counties.csv |jq '[.[] |  .cases = (.cases |(if . == "" then 0 else (.|tonumber) end) ) |.deaths = (.deaths|(if . == "" then 0 else (.|tonumber) end) )]' > nytimes-us-counties.json
-   #jq -s add last.json nytimes-us-counties.json  > covid-all.json
    node BigQuery.js -q 'select county_fips_code as fips, FORMAT_DATE("%F", date) as date , county, state_name as state, confirmed_cases as cases, deaths from `official.county-cases-all`' > covid-all.json
    datasplit covid-all.json fips $target_dir/
 }
 
 getCACountyVaccineData() {
-   target_dir='website/build/data/county'
+   target_dir='website/build/data/vaccine/counties'
    mkdir -p $target_dir
    node BigQuery.js -q 'select FORMAT_DATE("%F", date) as date, * except(date) from `official.CA-County-Vaccines-overtime`' > $target_dir/CA.json
    datasplit $target_dir/CA.json fips $target_dir/
