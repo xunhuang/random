67556bd7183216442cdcd4b2431192e4f65b1c4e
Wilson Huang (Growth) <xhuang@fb.com>
2021-04-04 12:01:33 -0700

    fix new site control

--- website/src/SubscriptionNew.tsx
+++ website/src/SubscriptionNew.tsx
@@ -6,16 +6,25 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
 import { useHistory } from "react-router-dom";
 import Routes from './Routes';
+import { CircularProgress, LinearProgress } from '@material-ui/core';
+const superagent = require('superagent');
 
 interface SubscriptionFormProperty {
     sub?: WatchSubscription;
     callback?: () => void;
 }
+
+const defaultsite = "https://news.ycombinator.com/";
+const scraperurl = "https://puppy-capsc6nslq-uc.a.run.app";
+// const scraperurl = "http://localhost:8080";
+
 export function SubscriptionNew(props: SubscriptionFormProperty) {
     let history = useHistory();
     let user = RandomBackend.getCurrentUser();
-    let [url, setURL] = React.useState("www.cnn.com");
+    let [url, setURL] = React.useState(defaultsite);
     let [dataSubmitted, setDataSubmitted] = React.useState(false);
+    let [pngBase64, setPngBase64] = React.useState(undefined);
+    let [loadingPreview, setLoadingPreview] = React.useState(false);
     type FormData = {
         url: string;
     };
@@ -27,20 +36,13 @@ export function SubscriptionNew(props: SubscriptionFormProperty) {
                 'Enter correct url!'
             )
             .required('Please enter website'),
-
     });
-    const { register, handleSubmit, errors, reset } = useForm<FormData>({
+    const { register, handleSubmit, errors, reset, getValues, trigger } = useForm<FormData>({
         resolver: yupResolver(schema)
     });
 
-    const onSubmit = handleSubmit(({ url }) => {
-        console.log(url);
+    const onRealSubmit = handleSubmit(({ url }) => {
         url = url.startsWith("http") ? url : "https://" + url;
-        /*
-        setURL(url);
-        setDataSubmitted(true);
-        */
-        // no pre-view for now just create the URL
         let sub = new WatchSubscription();
         sub.url = url;
         user.subscriptions.create(sub).then(function () {
@@ -53,12 +55,46 @@ export function SubscriptionNew(props: SubscriptionFormProperty) {
         });
     });
 
+    const onPreview = () => {
+        trigger().then((ok: boolean) => {
+            if (!ok) {
+                console.log("validation error");
+                return;
+            }
+            let url = getValues().url;
+            url = url.startsWith("http") ? url : "https://" + url;
+
+            console.log("loading " + url)
+
+            setLoadingPreview(true);
+            superagent.get(scraperurl)
+                .query({
+                    url: url
+                }).then((res: any) => {
+                    console.log(res);
+                    let body = JSON.parse(res.text);
+                    setLoadingPreview(false);
+                    setPngBase64(body.image);
+                });
+
+        });
+
+    };
+
     return (
         <div>
             <h1>Website to Watch?</h1>
-            <form onSubmit={onSubmit}>
-                <input name="url" ref={register} defaultValue={url} />
-                <input type="submit" />
+            <form onSubmit={e => { e.preventDefault(); }}>
+                <input name="url" ref={register} defaultValue={url} onKeyDown={
+                    function (e) {
+                        if (e.key === 'Enter') {
+                            onPreview();
+                        }
+                    }
+                } />
+                <button type="button"
+                    onClick={onPreview}>Preview</button>
+                <button type="button" onClick={onRealSubmit}>Subscribe</button>
                 <div>
                     {errors.url && "Please enter valid URL"}
                 </div>
@@ -70,6 +106,8 @@ export function SubscriptionNew(props: SubscriptionFormProperty) {
                     <iframe src={url}></iframe>
                 </div>
             }
+            {loadingPreview && <LinearProgress />}
+            {pngBase64 && <img src={`data:image/png;base64,${pngBase64}`} />}
 
         </div>
     );
